e57f2671b0e53aaf1f6cbbaaa44ad7674f061ac8
BlitzJB <blitz04.dev@gmail.com>
2025-04-24 13:12:30 +0530

    feat: actually remove tanstack query

--- app/providers.tsx
+++ app/providers.tsx
@@ -19,7 +19,6 @@ export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 } 
